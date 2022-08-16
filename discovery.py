4c1e6aed6eb0863c33e637259367af53991eee06
Tianji Cong <tianji@sigmacomputing.com>
2022-08-16 00:18:04 +0000

    Add play-around code of aurum discovery api

--- discovery.py
+++ discovery.py
@@ -2,10 +2,12 @@ from main import init_system
 
 
 if __name__ == "__main__":
-    model_dir_path = "/data/elasticsearch/testmodel/"
+    # model_dir_path = "/data/elasticsearch/spider/testmodel/"
+    model_dir_path = "/data/aurum_elasticsearch/nextiaJD-L/testmodel/"
     api, reporting = init_system(model_dir_path)
 
-    field = ("spider_csv_repository", "concert_singer+concert.csv", "name")
+    # field = ("spider_csv_repository", "concert_singer+concert.csv", "name")
+    field = ("nextiaJD-l_csv_repository", "library-collection-inventory.csv", "Title")
     drs = api.make_drs(field)
 
     res = api.content_similar_to(drs)
